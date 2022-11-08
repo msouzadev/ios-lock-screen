72a4aab0f624d852233cc49bad6753f791c4dd74
Matheus Souza <msouzadev@Matheuss-iMac.local>
2022-11-08 19:47:10 -0300

    lock screen background, and date time

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import wallpaper from "./assets/images/wallpaper.webp";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <ImageBackground source={wallpaper} style={styles.container}>
+      <View style={styles.header}>
+        <Ionicons name="ios-lock-closed" size={20} color="white" />
+        <Text style={styles.date}>Tuesday, 8 November</Text>
+        <Text style={styles.time}>15:26</Text>
+      </View>
       <StatusBar style="auto" />
-    </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+  },
+  header: {
+    justifyContent: "center",
+    height: 250,
+    alignItems: "center",
+  },
+  date: {
+    marginTop: 20,
+    color: "#C3FFFE",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  time: {
+    fontSize: 82,
+    fontWeight: "bold",
+    color: "#C3FFFE",
   },
 });
